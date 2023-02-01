fd06c592dcab726ed858bf59369c9519285ae85e
demurewolf <josiahwedgwood@ymail.com>
2023-02-01 14:00:07 -0500

    Added starting components with basic styling for them

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import './App.css';
 
+
+function TrackingMap() {
+  return (
+    <div className='tracking-map'>
+      <p>
+        I am a map that will show you the ISS.
+      </p>
+    </div>
+  )
+}
+
+function Stats() {
+  return (
+    <div className='stats'>
+      <p>
+        I will be a table of information
+      </p>
+    </div>
+  )
+}
+
+function Info() {
+  return (
+    <div className='info'>
+      <p>
+        I will be a list of fun facts about the ISS.
+      </p>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        ISS Tracking Map
       </header>
+      <div className='iss-information'>
+        <TrackingMap />
+        <Stats />
+        <Info />
+      </div>
     </div>
   );
 }
