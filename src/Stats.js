22135d92058e29c5ebef9fb8ba4ebbdf87ec8de4
demurewolf <josiahwedgwood@ymail.com>
2023-02-05 22:27:39 -0500

    Filtered and formatted some of the api data when showing the current iss stats table

--- src/Stats.js
+++ src/Stats.js
@@ -11,15 +11,37 @@ function StatTableRow({type, data}) {
   
 export default function Stats({currentStats}) {
     const rows = [];
+    
+    // Filter some stats and make values human readable
+    const STATS_FILTER_LIST = ["visibility", "solar_lat", "solar_lon", "units", "name", "id", "daynum", "footprint"];
+    const STATS_PRECISION_LIST = ["latitude", "longitude", "velocity", "altitude"];
     if (currentStats) {
-        Object.keys(currentStats).forEach(k => 
-            rows.push(<StatTableRow type={k} data={currentStats[k]}/>)
-        );
+      let speedUnit = currentStats["units"] === "miles" ? " mph" : " kph";
+      Object.keys(currentStats)
+        .forEach((k) => {
+          let statData = currentStats[k];
+          if (STATS_PRECISION_LIST.includes(k)) {
+            statData = currentStats[k].toFixed(3);
+          }
+          if (k === "altitude") {
+            statData = statData + " " + currentStats["units"];
+          }
+          if (k === "velocity") {
+            statData = statData + speedUnit;
+          }
+          if (k === "timestamp") {
+            statData = Date(currentStats[k]).toString();
+          }
+          if (!STATS_FILTER_LIST.includes(k)) { 
+            rows.push(<StatTableRow type={k} data={statData}/>);
+          }
+        }
+      );
     }
     return (
       <div className='stats'>
         <table>
-          <caption>ISS Statistics</caption>
+          <caption>Real Time ISS Statistics</caption>
           <thead>
             <tr>
               <th>Type</th>
