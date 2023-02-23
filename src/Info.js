af719bab5a8e77fc2efebcace312ff30b585274b
demurewolf <josiahwedgwood@ymail.com>
2023-02-23 13:24:38 -0500

    Completed fun info section that shows the current astronauts on the ISS

--- src/Info.js
+++ src/Info.js
@@ -1,11 +1,49 @@
+import { useEffect, useState } from "react";
+
+function AstroRow({name}) {
+  return (
+    <tr>
+      <td>{name}</td>
+    </tr>
+  );
+}
 
 export default function Info() {
-    return (
-      <div className='info'>
-        <p>
-          I will be a list of fun facts about the ISS.
-        </p>
-      </div>
-    )
+
+  const [astrosList, setAstrosList] = useState(null);
+  const [astrosNum, setAstrosNum] = useState(null);
+  const INFO_API_URL = "http://api.open-notify.org/astros.json";
+
+  useEffect(() => {
+    let ignore = false;
+    fetch(INFO_API_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        if (!ignore) {
+          console.log(data)
+          setAstrosNum(data["number"]);
+          let newAstros = []
+          data["people"].forEach((obj) => {
+            newAstros.push(<AstroRow name={obj["name"]}/>);
+          });
+          setAstrosList(newAstros);
+        }
+      });
+    
+    return () => {
+      ignore = true;
+    }
+  }, [INFO_API_URL]);
+
+  return (
+    <div className='info'>
+      <table>
+          <caption>Current {astrosNum} ISS Astronauts</caption>
+          <tbody>
+            {astrosList}
+          </tbody>
+        </table>
+    </div>
+  )
 }
   
