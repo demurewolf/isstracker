aa16285643540d901056cef07ead723d2372b85b
Josiah Wedgwood <josiahwedgwood@ymail.com>
2023-02-24 14:44:39 -0500

    backend now serves react frontend from root route

--- backend/iss/api.py
+++ backend/iss/api.py
@@ -2,8 +2,9 @@ import ephem
 from os.path import getmtime
 from subprocess import call
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 CURRENT_TLE_FILE = "current-tle.txt"
 app = FastAPI()
@@ -36,6 +37,13 @@ if not last_modified_time or getmtime(CURRENT_TLE_FILE) > last_modified_time:
     iss_tle = ephem.readtle(name, line1, line2)
     # help(iss)
 
+"""
+iss_tle data:
+iss_tle.n is in revolutions/day
+Need to approximate a landspeed
+iss_tle.elevation is in meters by default
+Only need to convert if requested units is in miles
+"""
 @app.get("/now")
 async def reIssData():
     iss_tle.compute()
@@ -47,12 +55,14 @@ async def reIssData():
         "eclipsed": iss_tle.eclipsed
     }
 
-@app.get("/", tags=["root"])
-async def root():
-    return {"message": "Hello World"}
-
-
-
-
-
-
+class SPAStaticFiles(StaticFiles):
+    async def get_response(self, path: str, scope) -> Response:
+        try:
+            return await super().get_response(path, scope)
+        except HTTPException as ex:
+            if ex.status_code == 404:
+                return await super().get_response("index.html", scope)
+            else:
+                raise ex
+    
+app.mount("/", SPAStaticFiles(directory="dist", html=True), name="app")
