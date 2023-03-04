5ef97fbb6dff1493a3ae5f3e0bb1a9e717b2c739
Josiah Wedgwood <josiahwedgwood@ymail.com>
2023-03-04 08:31:51 -0500

    Now using backend service to fill out names

--- frontend/src/Info.js
+++ frontend/src/Info.js
@@ -12,7 +12,7 @@ export default function Info() {
 
   const [astrosList, setAstrosList] = useState(null);
   const [astrosNum, setAstrosNum] = useState(0);
-  const INFO_API_URL = "http://api.open-notify.org/astros.json";
+  const INFO_API_URL = "/api/astros?craft=ISS";
 
   useEffect(() => {
     let ignore = false;
@@ -22,15 +22,10 @@ export default function Info() {
         if (!ignore) {
           setAstrosNum(data["number"]);
           let newAstros = [];
-          let issAstroCount = 0;
           data["people"].forEach((astro) => {
-            if (astro["craft"] === "ISS") {
-              newAstros.push(<AstroRow name={astro["name"]}/>);
-              issAstroCount ++;
-            }
+            newAstros.push(<AstroRow name={astro["name"]}/>);
           });
           setAstrosList(newAstros);
-          setAstrosNum(issAstroCount);
         }
       });
     
