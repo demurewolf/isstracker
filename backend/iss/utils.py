ff3f0c0eaddfabc163ec4507d5377bc484ce57e7
Josiah Wedgwood <josiahwedgwood@ymail.com>
2023-03-06 18:40:54 -0500

    Utils now gets updated tle from celestrak on server startup

--- backend/iss/utils.py
+++ backend/iss/utils.py
@@ -6,8 +6,8 @@ from requests import get
 
 import json
 
+CELESTRAK_TLE_URL = "https://celestrak.org/NORAD/elements/gp.php?GROUP=stations&FORMAT=tle"
 ASTRO_NOTIFY_API_URL = "http://api.open-notify.org/astros.json"
-CURRENT_TLE_FILE = "current-tle.txt"
 
 KILOMETERS_TO_MILE_FACTOR = 0.6213711922 # 0.6213711922 miles to 1 km
 
@@ -27,24 +27,30 @@ def angularv_to_linearv(rev_per_day, altitude, metric_units=True):
 def convert_elevation(elevation, metric_units=True):
     return elevation if metric_units else elevation * KILOMETERS_TO_MILE_FACTOR / 1000
 
-def setup_tle(tle_file: str = "current-tle.txt"):
+def update_tle_data():
+    celestrak_resp = get(CELESTRAK_TLE_URL)
+    iss_tle_data = []
+
+    if celestrak_resp.ok:
+        all_tle_lines = celestrak_resp.text.split("\r\n")
+        
+        # ISS is always first in the list
+        for i in range(3):
+            iss_tle_data.append(all_tle_lines[i])
+
+    return iss_tle_data   
+
+
+def setup_tle():
     last_modified_time = 0
-    call(["get-tle"])
 
-    if not last_modified_time or getmtime(CURRENT_TLE_FILE) > last_modified_time:
-        print("TLE has more recent time")
-        last_modified_time = getmtime(CURRENT_TLE_FILE)
-        with open(CURRENT_TLE_FILE) as tle_file:
-            name = tle_file.readline().rstrip()
-            line1 = tle_file.readline().rstrip()
-            line2 = tle_file.readline().rstrip()
-
-    return readtle(name, line1, line2)
+    tle_data = update_tle_data()
+    return readtle(tle_data[0], tle_data[1], tle_data[2])
 
 def setup_astros():
     astros_api_resp = get(ASTRO_NOTIFY_API_URL)
     astros_list = []
     if astros_api_resp.ok:
         astros_list = json.loads(astros_api_resp.text)["people"]
-        print(astros_list)
+    
     return astros_list
